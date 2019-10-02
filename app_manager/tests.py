39038e835db5f49d4bbb39f8c91ae30e285bdaf4
sujon-kutumbita <sujon@kutumbita.com>
2019-10-02 10:33:46 +0600

    update code

---
+++ app_manager/tests.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+import unittest
+
+
+class TestBasic(unittest.TestCase):
+    "Basic tests"
+
+    def test_basic(self):
+        a = 1
+        self.assertEqual(1, a)
+
+    def test_basic_2(self):
+        a = 1
+        assert a == 1
